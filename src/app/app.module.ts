3fbf52a48fc7cd1f52287492283a4faa1493916b
Kevin Yang <chgc_kevin@hotmail.com>
2019-09-23 15:56:23 +0800

    first draft web page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CfpComponent } from './cfp/cfp.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CfpComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cfp/cfp.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gdg-cfp',
+  templateUrl: './cfp.component.html',
+  styleUrls: ['./cfp.component.scss']
+})
+export class CfpComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
